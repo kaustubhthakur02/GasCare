c9338a8cf9dc77e2ba73e5b89303144f650bb60b
kaustubhthakur02 <thakurkaustubh37@gmail.com>
2024-04-23 19:51:07 +0530

    add static and template files

--- GasCare/GasCare/settings.py
+++ GasCare/GasCare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'GasCare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +71,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'GasCare.wsgi.application'
 
+
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases

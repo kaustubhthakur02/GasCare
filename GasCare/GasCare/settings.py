5ac572d0a621920a75cd818bac1e6230ac48ac69
kaustubhthakur02 <thakurkaustubh37@gmail.com>
2024-04-23 19:41:38 +0530

    change database to mysql

--- GasCare/GasCare/settings.py
+++ GasCare/GasCare/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'GasCare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gascare',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD':'',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
